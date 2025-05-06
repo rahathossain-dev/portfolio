638c05aab4ae35c3791062af79ced3ad90f8fded
Rahat Hossain <rahat1470.com@gmail.com>
2025-05-06 14:08:31 +0600

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import App from "./App";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const routes = createBrowserRouter([
   {
     path: '/',
